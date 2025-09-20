74ad2afef90b757fdfa5e754aa530dd0db1e98c2
Arthur7567 <miguepg06@gmail.com>
2025-09-19 18:48:22 -0600

    Arthur Files

---
+++ htdocs/inicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <link rel="stylesheet" href="general.css">
+    <?php 
+        include "nav_bar.php"
+    ?>
+    <div class="content">
+    <div class="top-bar">
+      <div class="admin-profile">
+        <span class="admin-name">Admin - Luis Perez Ponzo</span>
+        <img src="img/admin.png" alt="Perfil Admin">
+      </div>
+    </div>
+        
+        <h1>Coffee shop</h1> <h1></h1>
+        <h1>¡Se le da la bienvenida al panel de control!</h1>
+        <h1>¿Retomamos desde donde lo dejamos?<h1>
+    </div>
+</body>
+</html>
